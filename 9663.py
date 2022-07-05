cf63c02db88d04c05d3f308a0dc83351ec5794cc
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-07-05 13:24:09 +0900

    9663-N-Queen(백트래킹)

---
+++ 9663.py
@@ -0,0 +1,21 @@
+N = int(input())
+row = [0] * N
+result = 0
+
+def check(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == x - i:
+            return False
+    return True
+
+def dfs(x):
+    global result
+    if x == N:
+        result += 1
+    else:
+        for i in range(N):
+            row[x] = i
+            if check(x):
+                dfs(x+1)
+dfs(0)
+print(result)
