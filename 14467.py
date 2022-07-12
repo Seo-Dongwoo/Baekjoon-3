4845791884b158f317056b33cacacc25cc517f1e
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-07-12 14:07:55 +0900

    14467-소길건너는이유(구현)

---
+++ 14467.py
@@ -0,0 +1,14 @@
+N = int(input())
+cow = [-1]*11
+cnt = 0
+for _ in range(N):
+    target_cow, position = map(int, input().split(' '))
+
+    if cow[target_cow] == -1:
+        cow[target_cow] = position
+    else:
+        if cow[target_cow] != position:
+            cnt += 1
+            cow[target_cow] = position
+
+print(cnt)
