a5ec51eac560c41d188de3a110fa0c62ce61c85d
SeoDongwoo <83106932+Seo-Dongwoo@users.noreply.github.com>
2022-07-14 17:28:48 +0900

    10026-적록색약(bfs)

---
+++ 10026.py
@@ -0,0 +1,44 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    visited[x][y] = cnt
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N:
+                if graph[nx][ny] == graph[x][y] and visited[nx][ny] == 0:
+                    queue.append((nx,ny))
+                    visited[nx][ny] = 1
+                    
+N = int(input())
+graph = [list(map(str, input())) for _ in range(N)]
+visited = [[0]*N for _ in range(N)]
+cnt = 0
+for i in range(N):
+    for j in range(N):
+        if visited[i][j] == 0:
+            bfs(i, j)
+            cnt += 1
+print(cnt, end=' ')
+
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == "R":
+            graph[i][j] = "G"
+visited = [[0]*N for _ in range(N)]
+
+cnt = 0
+for i in range(N):
+    for j in range(N):
+        if visited[i][j] == 0:
+            bfs(i,j)
+            cnt += 1
+print(cnt)
